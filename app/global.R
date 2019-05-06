c4ced60dbcda44868108baf4b86775c66e66562a
strayMat <matt.dout@gmail.com>
2019-05-06 23:59:55 +0200

    added unit tests with test that and refacto app code to match the minimum of functionnal programming necessary to have tests

---
+++ app/global.R
@@ -0,0 +1,16 @@
+# global with dependencies and common paths
+
+library(dplyr)
+library(ggplot2) 
+library(grid)
+library(gridExtra) 
+library(markdown)
+library(networkD3)
+library(shiny)
+
+
+# Data
+
+PATH2MARKDOWNS = paste0("www/markdowns/")
+
+source("functions.R")
