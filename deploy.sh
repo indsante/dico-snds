765dd9b7de3388623884fa5bf373223a343c15ad
strayMat <matt.dout@gmail.com>
2019-05-07 15:19:56 +0200

    change rsconnect installation of stage to put it in deploy stage

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 # Deploy
+apt-get update && apt-get install -y libv8-3.14-dev libcurl4-openssl-dev libssl-dev
+r -e "install.packages(c('rsconnect'), repos='http://cran.rstudio.com/')"
 r -e "rsconnect::setAccountInfo(name='drees', token='${TOKEN}', secret='${SECRET}')"
 r -e "rsconnect::deployApp(appDir = 'app', appName = 'dico-snds')"
--- install_Rpackages.sh
+++ install_Rpackages.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-apt-get update && apt-get install -y libv8-3.14-dev libcurl4-openssl-dev libssl-dev
-r -e "install.packages(c('backports', 'crosstalk', 'dplyr', 'DT', 'evaluate', 'ggplot2', 'gridExtra', 'highr', 'knitr', 'markdown', 'networkD3', 'rmarkdown', 'rprojroot', 'rsconnect', 'shiny', 'testthat'), repos='http://cran.rstudio.com/')"
+r -e "install.packages(c('backports', 'crosstalk', 'dplyr', 'DT', 'evaluate', 'ggplot2', 'gridExtra', 'highr', 'knitr', 'markdown', 'networkD3', 'rmarkdown', 'rprojroot', 'shiny', 'testthat'), repos='http://cran.rstudio.com/')"
