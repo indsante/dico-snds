4a40b71bd227bfb699e998f7de2898770ba87f09
Matthieu Doutreligne <matt.dout@gmail.com>
2019-10-25 17:20:02 +0000

    Fix package mongo

--- install_Rpackages.sh
+++ install_Rpackages.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-r -e "install.packages(c('backports', 'clipr', 'crosstalk', 'dplyr', 'DT', 'elastic', 'evaluate', 'ggplot2', 'gridExtra', 'highr', 'knitr', 'markdown', 'networkD3', 'rmarkdown', 'rprojroot', 'shiny', 'testthat', 'tidyr'), repos='http://cran.rstudio.com/')"
+r -e "install.packages(c('backports', 'clipr', 'crosstalk', 'dplyr', 'DT', 'elastic', 'evaluate', 'ggplot2', 'gridExtra', 'highr', 'knitr', 'markdown', 'mongolite', 'networkD3', 'rmarkdown', 'rprojroot', 'shiny', 'testthat', 'tidyr'), repos='http://cran.rstudio.com/')"
