5e90f5d48eddb3ab47d87651127a2066a4d6be55
Phileas Condemine <phileas.condemine@sante.gouv.fr>
2019-10-28 08:34:09 +0000

    fix dep linux pour mongolite

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # Deploy
-apt-get update && apt-get install -y libv8-3.14-dev libcurl4-openssl-dev libssl-dev xclip
+apt-get update && apt-get install -y libv8-3.14-dev libcurl4-openssl-dev libssl-dev xclip libsasl2-dev
 r -e "install.packages(c('backports', 'crosstalk', 'clipr' , 'dplyr', 'DT', 'elastic', 'evaluate', 'ggplot2', 'gridExtra', 'highr', 'knitr', 'markdown', 'mongolite', 'networkD3', 'rmarkdown', 'rprojroot', 'rsconnect', 'shiny', 'testthat', 'tidyr'), repos='http://cran.rstudio.com/')"
 r -e "rsconnect::setAccountInfo(name='drees', token='${TOKEN}', secret='${SECRET}')"
 export API_TOKEN=${MONGO_TOKEN}
