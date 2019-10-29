4ab47dd77c78acc2903bb69a68705ad7a839a299
Phileas Condemine <phileas.condemine@sante.gouv.fr>
2019-10-29 08:49:43 +0000

    need curl for front-test

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # Deploy
-apt-get update && apt-get install -y libv8-3.14-dev libcurl4-openssl-dev libssl-dev xclip libsasl2-dev zlib1g-dev
+apt-get update && apt-get install -y libv8-3.14-dev libcurl4-openssl-dev libssl-dev xclip libsasl2-dev zlib1g-dev curl
 r -e "install.packages(c('backports', 'crosstalk', 'clipr' , 'dplyr', 'DT', 'elastic', 'evaluate', 'ggplot2', 'gridExtra', 'highr', 'knitr', 'markdown', 'mongolite', 'networkD3', 'rmarkdown', 'rprojroot', 'rsconnect', 'shiny', 'testthat', 'tidyr'), repos='http://cran.rstudio.com/')"
 r -e "rsconnect::setAccountInfo(name='drees', token='${TOKEN}', secret='${SECRET}')"
 echo ${MONGO_TOKEN} > app/server/mongo_password.txt
