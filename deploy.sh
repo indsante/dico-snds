b2e95f40f4dbe7fa9402376d3edb5e17a016b53a
El Oualydy Salma <salma.eloualydy@health-data-hub.fr>
2020-12-15 10:35:47 +0000

    Ajout de la gestion des cookies et mise à jour du README

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 apt-get update && apt-get install -y libv8-3.14-dev libcurl4-openssl-dev libssl-dev xclip libsasl2-dev zlib1g-dev
 r -e "install.packages(c('backports', 'crosstalk', 'clipr' , 'dplyr', 'DT', 'elastic', 'evaluate', 'ggplot2', 'gridExtra', 'highr', 'knitr', 'markdown', 'mongolite', 'networkD3', 'rmarkdown', 'rprojroot', 'rsconnect', 'shiny', 'testthat', 'tidyr'), repos='http://cran.rstudio.com/')"
-r -e "rsconnect::setAccountInfo(name='drees', token='${TOKEN}', secret='${SECRET}')"
+r -e "rsconnect::setAccountInfo(name='health-data-hub', token='${TOKEN}', secret='${SECRET}')"
 echo "MONGO_PWD=${MONGO_TOKEN}" > app/server/var_env.txt
 echo "MONGO_HOST=${MONGO_HOST}" >> app/server/var_env.txt
 echo "MONGO_USERNAME=${MONGO_USERNAME}" >> app/server/var_env.txt
